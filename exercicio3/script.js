30893d2651294ddbfc1a2b72835180c5241f16f7
karineveppo <karineveppo83@gmail.com>
2022-09-06 17:49:28 -0300

    exercícios condicionaisI a012

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,30 @@
-let nacionalidade //= adicione o prompt aqui
+//Crie um código que receba um **prompt** que diga: "Escreva aqui sua nacionalidade" 
+//e guarde o valor da resposta em uma const `nacionalidade`:
+
+const nacionalidade = prompt("Escreva aqui sua nacionalidade") 
+
+//Crie uma estrutura de `if/else if/else` 
+//que verifique se a nacionalidade guardada na const `nacionalidade` 
+//é igual a cada uma das nacionalidades acima, e caso seja, 
+//imprima a nacionalidade no console:
+
+if (nacionalidade === "brasileira" ){
+    console.log("nacionalidade encontrada")
+
+} else if(nacionalidade === "argentina" ){
+    console.log("nacionalidade encontrada")
+
+}else if (nacionalidade === "uruguaia"){
+    console.log("nacionalidade encontrada")
+
+} else if (nacionalidade === "chilena"){
+     console.log("nacionalidade encontrada")
+
+} else if (nacionalidade === "colombiana"){
+     console.log("nacionalidade encontrada")
+
+} else {
+     console.log("nacionalidade não encontrada!")}
+
+//O último `else` deve imprimir "nacionalidade não encontrada" 
+//caso o valor de `nacionalidade` não corresponda a nenhum dos valores.
